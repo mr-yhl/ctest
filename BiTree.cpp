1be5ff4e9e25cf2aeeae89b07a999bcbe41f1844
mr-yhl <jyanghailin@163.com>
2019-12-05 11:37:51 +0800

    二叉树

---
+++ BiTree.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+//单链表；
+typedef char ElemType;
+typedef struct node{
+	ElemType data;
+	struct node *lch,*rch;
+}BiBode,*BiTree; 
+int main(){
+	char a[8]={' ','A','B','C',' ','D',' ',' '};//一维数组 
+}
